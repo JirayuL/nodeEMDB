7c2d39f95269067b33422378169160ff56757a75
Jirayu Laungwilawan <jirayu.l@ku.th>
2018-09-04 12:39:43 +0700

    Update

--- index.js
+++ index.js
@@ -2,18 +2,89 @@ var express = require('express')
 var path = require('path')
 var app = express()
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodekb');
+var db = mongoose.connection;
+db.on('open', function () { console.log('connected to MongoDB.') });
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.get('/', function (req, res) {
-  res.render('index')
-})
+const Article = require('./models/article');
 
-app.get('/news/add', function (req, res) {
-  res.render('add_news', {
-    title: 'News'
+//Route
+app.get('/', function (req, res) {
+  Article.find({}, function (err, news) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: 'Home',
+        news: news
+      });
+    }
+  });
+});
+
+// app.get('/', function (req, res) {
+  //   res.render('index')
+  // })
+  
+app.get('/', function (req, res) {
+  let news = [{
+      id: 1,
+      title: 'news1',
+      body: 'this is news 1.'
+    },
+    {
+      id: 2,
+      title: 'news2',
+      body: 'this is news 2.'
+    },
+    {
+      id: 3,
+      title: 'news3',
+      body: 'this is news 3.'
+    }
+  ];
+  res.render('index', {
+    title: 'Home',
+    news: news
   })
 })
+
+
+// app.get('/news/add', function (req, res) {
+//   res.render('add_news', {
+//     title: 'News'
+//   })
+// })
+app.get('/news/add', function (req, res) {
+  res.render('add_news', {
+    title: 'Add news'
+  })
+})
+
+app.post('/news/add', function (req, res) {
+  let news = new Article()
+  news.title = req.body.title
+  news.body = req.body.body
+  news.save(function (err) {
+    if (err) {
+      console.log(err)
+      return
+    } else {
+      res.redirect('/')
+    }
+  })
+});
 
 
 app.post('/', function (req, res) {
